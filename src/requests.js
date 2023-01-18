0815f4e199481d86cc47e5eec7d5bdb330499f88
Vivek Gundu <62993315+Vivek0624@users.noreply.github.com>
2023-01-18 12:58:52 +0530

    inital commit
    Added files to github

---
+++ src/requests.js
@@ -0,0 +1,16 @@
+// https://api.themoviedb.org/3/movie/550?api_key=37cbdb0992deb527dd129d66d9e5d0c0
+const API_KEY = "37cbdb0992deb527dd129d66d9e5d0c0";
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+export default requests;
